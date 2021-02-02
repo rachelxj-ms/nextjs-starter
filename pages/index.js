4db05673a33d7cf82852cf8cae7f007481ee6e45
rachelxj-ms <xuejl@microsoft.com>
2021-02-02 13:23:25 +0800

    add api

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { projects } from '../utils/projectsData';
 const Home = () => (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
-    <h2><a href="/api/message?name=StaticWebApp">Hello, Static Web App</a></h2>
+    <h2><a href="/api/message?name=World">Hello, World</a></h2>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
